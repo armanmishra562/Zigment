dad759c8509a9949a461943d47019ee95912004f
armanmishra562 <arman2004562@gmail.com>
2024-11-18 15:23:58 +0530

    api-auth-testing

--- src/common/middleware/api-key.middleware.ts
+++ src/common/middleware/api-key.middleware.ts
@@ -5,8 +5,8 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class ApiKeyMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    const apiKey = req.header('API_KEY');
-    if (apiKey !== process.env.API_KEY) {
+    const apiKey = req.header('API_KEYs');
+    if (apiKey !== process.env.API_KEYs) {
       throw new UnauthorizedException('Invalid API key');
     }
     next();
